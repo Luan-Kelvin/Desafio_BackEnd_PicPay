5f22dec85e0067be66da531ff8df5dbeb09386db
Luan Kelvin <kelvinluan897@gmail.com>
2026-09-14 12:01:36 -0300

    First commit

---
+++ src/test/java/com/picpaySimplificado/picpaySimplificado/PicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaySimplificado.picpaySimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
